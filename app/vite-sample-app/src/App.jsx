9ff1e7e0d4d8ed3f00751ee504932983b7ede736
Manami Kokado <kokado313.mnm@gmail.com>
2024-06-03 23:52:48 +0900

    React練習用Todoアプリ実装

--- app/vite-sample-app/src/App.jsx
+++ app/vite-sample-app/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleClick = () => {
+    if (todo) {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Todo App</h1>
+      <input
+        type="text"
+        placeholder="input your task..."
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button onClick={handleClick}>
+        Add Todo
+      </button>
+      {todos.map((element, index) => (
+        <div key={index}>
+          <input type="checkbox" id="index" />
+          <label htmlFor="checkbox">{element}</label>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
